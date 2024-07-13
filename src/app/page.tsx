d9bd51c7672e39a83f3591b26fbf57c3a234672f
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-07-13 19:34:50 +0600

    Add Hero component into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Hero from "@/components/UI/Hero/Hero";
+import Navbar from "@/components/UI/Navbar/Navbar";
+
 export default function Home() {
   return (
     <>
-
+      <Navbar />
+      <Hero />
     </>
   );
 }
