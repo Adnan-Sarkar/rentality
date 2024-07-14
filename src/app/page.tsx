90246bef82afa9406c444f51d5777fbfa2704b3b
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-07-14 22:43:45 +0600

    Add promotional video component into root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/UI/Hero/Hero";
 import Navbar from "@/components/UI/Navbar/Navbar";
+import PromotionalVideo from "@/components/UI/PromotionalVideo/PromotionalVideo";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <PromotionalVideo />
     </>
   );
 }
