72488653297b334fa9348d287b80bae87edc26c7
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-07-15 22:27:28 +0600

    Update root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
-import type {Metadata} from "next";
-import {Poppins} from "next/font/google";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
-const poppins = Poppins({weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Rentality",
